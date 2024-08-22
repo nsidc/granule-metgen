941494331f72cb6b3c2b4036b145ae72877b4b05
Kevin W. Beam <kevin.beam@colorado.edu>
2024-08-22 12:21:46 -0600

    Adds help text to the cli

--- src/nsidc/metgen/cli.py
+++ src/nsidc/metgen/cli.py
@@ -5,11 +5,13 @@ from nsidc.metgen import metgen
 
 @click.group()
 def cli():
+    """The instameta utility allows users to create granule-level metadata."""
     pass
 
 @cli.command()
 @click.option('--config', help='metgen configuration file')
 def init(config):
+    """Creates a new metgen config file based on user input."""
     click.echo(metgen.banner())
     metgen.init_config(config)
     click.echo(f'Initialized the metgen configuration file {config}')
@@ -17,6 +19,7 @@ def init(config):
 @cli.command()
 @click.option('--config', help='metgen configuration file')
 def info(config):
+    """Summarizes the values in a metgen configuration file."""
     click.echo(metgen.banner())
     configuration = metgen.configuration(metgen.config_parser(config))
     metgen.show_config(configuration)
@@ -24,6 +27,7 @@ def info(config):
 @cli.command()
 @click.option('--config', help='metgen configuration file')
 def process(config):
+    """Processes science data files based on a metgen configuration file."""
     click.echo(metgen.banner())
     configuration = metgen.configuration(metgen.config_parser(config))
     metgen.process(configuration)
