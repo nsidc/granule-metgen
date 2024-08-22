b49fbcc9fc3d88ff2f38852418b4c813c4af9597
Kevin W. Beam <kevin.beam@colorado.edu>
2024-08-22 12:09:14 -0600

    Extract two unit test fixtures

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,35 +1,35 @@
 from click.testing import CliRunner
+import pytest
 
 from nsidc.metgen.cli import cli
 
 
-def test_without_subcommand():
-    runner = CliRunner()
-    result = runner.invoke(cli)
+@pytest.fixture
+def cli_runner():
+    return CliRunner()
+
+def test_without_subcommand(cli_runner):
+    result = cli_runner.invoke(cli)
     assert result.exit_code == 0
     assert 'Usage' in result.output
     assert 'Commands' in result.output
     for subcommand in ['info', 'init', 'process']:
         assert subcommand in result.output
 
-def test_help():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['--help'])
+def test_help(cli_runner):
+    result = cli_runner.invoke(cli, ['--help'])
     assert result.exit_code == 0
 
-def test_info_requires_config():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['info'])
+def test_info_requires_config(cli_runner):
+    result = cli_runner.invoke(cli, ['info'])
     assert result.exit_code != 0
 
-def test_info_with_config():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['info', '--config', './example/modscg.ini'])
+def test_info_with_config(cli_runner):
+    result = cli_runner.invoke(cli, ['info', '--config', './example/modscg.ini'])
     assert result.exit_code == 0
 
-def test_info_with_config_summarizes():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['info', '--config', './example/modscg.ini'])
+def test_info_with_config_summarizes(cli_runner):
+    result = cli_runner.invoke(cli, ['info', '--config', './example/modscg.ini'])
 
     for section in ['Source', 'Destination']:
         assert section in result.output
--- tests/test_metgen.py
+++ tests/test_metgen.py
@@ -6,6 +6,16 @@ import pytest
 from nsidc.metgen import metgen
 
 
+@pytest.fixture
+def cfg_parser():
+    cp = ConfigParser()
+    cp['Source'] = { 'data_dir': '/data/example' }
+    cp['Destination'] = {
+        'kinesis_arn': 'abcd-1234',
+        's3_url': 's3://example/xyzzy'
+    }
+    return cp
+
 def test_banner():
     assert len(metgen.banner()) > 0
 
@@ -19,26 +29,12 @@ def test_config_parser_return_type(mock):
 
     assert isinstance(result, ConfigParser)
 
-def test_config_from_config_parser():
-    cfg_parser = ConfigParser()
-    cfg_parser['Source'] = { 'data_dir': 'bar' }
-    cfg_parser['Destination'] = {
-        'kinesis_arn': 'abcd-1234',
-        's3_url': 's3://example/xyzzy'
-    }
-
+def test_config_from_config_parser(cfg_parser):
     config = metgen.configuration(cfg_parser)
 
     assert isinstance(config, metgen.Config)
 
-def test_config_with_values():
-    cfg_parser = ConfigParser()
-    cfg_parser['Source'] = { 'data_dir': '/data/example' }
-    cfg_parser['Destination'] = {
-        'kinesis_arn': 'abcd-1234',
-        's3_url': 's3://example/xyzzy'
-    }
-
+def test_config_with_values(cfg_parser):
     config = metgen.configuration(cfg_parser)
 
     assert config.source_data_dir == '/data/example'
