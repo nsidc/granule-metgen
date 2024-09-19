6099e18c84b6eaf0983c060f701dcf99dc1088d3
Kevin W. Beam <kevin.beam@colorado.edu>
2024-09-19 12:05:34 -0600

    Add docstrings and cleanup code.

--- src/nsidc/metgen/aws.py
+++ src/nsidc/metgen/aws.py
@@ -1,10 +1,17 @@
 import boto3
 
 
+KINESIS_PARTITION_KEY = "metgenc-duck"
+
 def post_to_kinesis(kinesis_stream_arn, cnm_message):
+    """Posts a message to a Kinesis stream."""
     client = boto3.client("kinesis", region_name="us-west-2")
     try:
-        result = client.put_record(StreamARN=kinesis_stream_arn, Data=cnm_message, PartitionKey="duck")
+        result = client.put_record(
+            StreamARN=kinesis_stream_arn,
+            Data=cnm_message,
+            PartitionKey=KINESIS_PARTITION_KEY
+        )
         print(f'Published CNM message {cnm_message} to stream ARN: {kinesis_stream_arn}')
         return result['ShardId']
     except Exception as e:
--- tests/test_aws.py
+++ tests/test_aws.py
@@ -32,24 +32,29 @@ def kinesis_stream_arn(kinesis):
 
 @pytest.fixture
 def test_message():
+    """Returns a JSON string for testing."""
     return json.dumps({
         'foo': 333,
         'bar': 'xyzzy'
     })
 
 def test_post_to_kinesis(kinesis_stream_arn, test_message):
+    """Given a Kinesis stream ARN and a message, it should post successfully."""
     success = aws.post_to_kinesis(kinesis_stream_arn, test_message)
     assert type(success) is str
 
 def test_post_to_kinesis_returns_foo(kinesis_stream_arn, test_message):
+    """Given a Kinesis stream ARN and a test message, the response should include the shard id."""
     result = aws.post_to_kinesis(kinesis_stream_arn, test_message)
     assert "shardId" in result
 
 def test_post_to_kinesis_with_invalid_stream_arn(kinesis_stream_arn, test_message):
+    """Given an invalid Kinesis stream ARN and a message, it should raise an exception."""
     invalid_stream_arn = "abcd-1234-wxyz-0987"
     with pytest.raises(Exception):
         aws.post_to_kinesis(invalid_stream_arn, test_message)
 
 def test_post_to_kinesis_with_empty_message(kinesis_stream_arn):
+    """Given a Kinesis stream ARN, it should raise an exception when posting an empty message."""
     with pytest.raises(Exception):
         aws.post_to_kinesis(kinesis_stream_arn, None)
