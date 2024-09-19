f1d400b57212f905c67610d589b48e0c29a0cd6f
Kevin W. Beam <kevin.beam@colorado.edu>
2024-09-19 11:09:35 -0600

    Add aws unit tests

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,8 +1,8 @@
 import json
 
-import pytest
 import boto3
 from moto import mock_aws
+import pytest
 
 from nsidc.metgen import aws
 
@@ -18,4 +18,31 @@ def test_post_to_kinesis():
         'bar': 'xyzzy'
     })
 
-    aws.post_to_kinesis(stream_arn, test_message)
+    success = aws.post_to_kinesis(stream_arn, test_message)
+
+    assert success == True
+
+@mock_aws
+def test_post_to_kinesis_with_invalid_stream_arn():
+    client = boto3.client("kinesis", region_name="us-west-2")
+    client.create_stream(StreamName="duck-test-stream", ShardCount=1)
+    summary = client.describe_stream_summary(StreamName="duck-test-stream")
+    stream_arn = summary['StreamDescriptionSummary']['StreamARN']
+    test_message = json.dumps({
+        'foo': 333,
+        'bar': 'xyzzy'
+    })
+    invalid_stream_arn = "abcd-1234-wxyz-0987"
+
+    with pytest.raises(Exception):
+        aws.post_to_kinesis(invalid_stream_arn, test_message)
+
+@mock_aws
+def test_post_to_kinesis_with_empty_message():
+    client = boto3.client("kinesis", region_name="us-west-2")
+    client.create_stream(StreamName="duck-test-stream", ShardCount=1)
+    summary = client.describe_stream_summary(StreamName="duck-test-stream")
+    stream_arn = summary['StreamDescriptionSummary']['StreamARN']
+
+    with pytest.raises(Exception):
+        aws.post_to_kinesis(stream_arn, None)
