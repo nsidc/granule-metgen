18a34944cc8a341d6c13e5ec7206f9559d2231d1
Kevin W. Beam <kevin.beam@colorado.edu>
2024-11-27 11:42:46 -0700

    Don't validate AWS params in dry_run mode

--- src/nsidc/metgen/config.py
+++ src/nsidc/metgen/config.py
@@ -44,6 +44,7 @@ class Config:
         if self.dry_run:
             LOGGER.info('')
             LOGGER.info('Note: The dry-run option was included, so no files will be staged and no CNM messages published.')
+            LOGGER.info('')
 
     def ummg_path(self):
         return Path(self.local_output_dir, self.ummg_dir)
@@ -122,12 +123,18 @@ def validate(configuration):
     Validates each value in the configuration.
     """
     validations = [
-        ['data_dir', lambda dir: os.path.exists(dir), 'The data_dir does not exist.'],
-        ['local_output_dir', lambda dir: os.path.exists(dir), 'The local_output_dir does not exist.'],
-        # ['ummg_dir', lambda dir: os.path.exists(dir), 'The ummg_dir does not exist.'],  ## validate "local_output_dir/ummg_dir" as part of issue-71
-        ['kinesis_stream_name', lambda name: aws.kinesis_stream_exists(name), 'The kinesis stream does not exist.'],
-        ['staging_bucket_name', lambda name: aws.staging_bucket_exists(name), 'The staging bucket does not exist.'],
-        ['number', lambda number: 0 < number, 'The number of granules to process must be positive.'],
+        ['data_dir', lambda dir: os.path.exists(dir),
+         'The data_dir does not exist.'],
+        ['local_output_dir', lambda dir: os.path.exists(dir),
+         'The local_output_dir does not exist.'],
+        # ['ummg_dir', lambda dir: os.path.exists(dir),
+        #  'The ummg_dir does not exist.'],  ## validate "local_output_dir/ummg_dir" as part of issue-71
+        ['kinesis_stream_name', lambda name: configuration.dry_run or aws.kinesis_stream_exists(name),
+         'The kinesis stream does not exist.'],
+        ['staging_bucket_name', lambda name: configuration.dry_run or aws.staging_bucket_exists(name),
+         'The staging bucket does not exist.'],
+        ['number', lambda number: 0 < number,
+         'The number of granules to process must be positive.'],
     ]
     errors = [msg for name, fn, msg in validations if not fn(getattr(configuration, name))]
     if len(errors) == 0:
