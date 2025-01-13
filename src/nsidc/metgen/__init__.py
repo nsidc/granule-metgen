b897bd4d9b766b921384c7ca16bc405c228538a4
Kevin W. Beam <kevin.beam@colorado.edu>
2025-01-13 15:24:02 -0700

    Update package imports

--- src/nsidc/metgen/__init__.py
+++ src/nsidc/metgen/__init__.py
@@ -1,4 +1,11 @@
 __version__ = "v1.0.0"
 
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "aws", "cli", "config", "constants", "metgen", "netcdf_reader"]
+
+from . import aws
+from . import cli
+from . import config
+from . import constants
+from . import metgen
+from . import netcdf_reader
