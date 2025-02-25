c649e958a82d6074307dd4e3235538895ec2df59
Julia Collins <collinsj@nsidc.org>
2025-02-24 18:22:11 -0700

    update text fixture

--- tests/test_config.py
+++ tests/test_config.py
@@ -36,6 +36,8 @@ def expected_keys():
             "time_coverage_duration",
             "pixel_size",
             "date_modified",
+            "browse_regex",
+            "granule_regex"
         ]
     )
 
