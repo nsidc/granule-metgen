6f8ddd397b04eeb511ce766f1c0d4675a586c503
Julia Collins <collinsj@nsidc.org>
2025-03-26 10:31:49 -0600

    Bump version: 1.4.0rc3 → 1.4.0rc4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ sys.path.insert(0, str(Path("..", "..", "src").resolve()))
 project = "MetGenC"
 copyright = "2024, NSIDC"
 author = "National Snow and Ice Data Center"
-release = "v1.4.0rc3"
-version = "v1.4.0rc3"
+release = "v1.4.0rc4"
+version = "v1.4.0rc4"
 
 extensions = [
     "myst_parser",
