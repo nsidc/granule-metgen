f1747a1673e1fa2f00203c122b40015a3679606b
Kevin W. Beam <kevin.beam@colorado.edu>
2025-03-27 10:03:01 -0600

    Fix registry.lookup function comment

--- src/nsidc/metgen/readers/registry.py
+++ src/nsidc/metgen/readers/registry.py
@@ -7,11 +7,7 @@ from nsidc.metgen.readers import csv, snowex_csv
 
 def lookup(collection: str, extension: str) -> Callable[[str, Config], dict]:
     """
-    Determine which file reader to use for the given collection and data
-    file extension. This currently is limited to handling one data file
-    type, and one reader. In a future issue, we may handle granules with
-    multiple data file types per granule. In that future work this needs
-    to be refactored to handle this case.
+    Determine which file reader to use for the given collection and data file extension.
     """
     special_readers = {"snex23": snowex_csv.extract_metadata}
 
